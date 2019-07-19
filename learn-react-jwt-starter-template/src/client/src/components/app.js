47bf596cd3a889a4c26c7087b72566cbced59f84
akbaral <azizakbaral@gmail.com>
2019-07-19 16:46:49 +0700

    add client view

---
+++ learn-react-jwt-starter-template/src/client/src/components/app.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import './app.css';
+
+
+class app extends Component {
+
+  /* Create a new instance of the 'AuthHelperMethods' compoenent*/
+  state = {
+    username: "",
+    password: ""
+  }
+
+/* Here will want to add a method to log the user out upon clicking 'Logout' */
+  _handleLogout = () => {
+    
+    
+    this.props.history.replace('/login');
+  }
+
+  //Render the protected component
+  render() {
+    let name = null;
+
+    //This will be null until we set up authentication...
+    if (this.props.confirm) {
+      name = this.props.confirm.username;
+    }
+
+    return (
+      <div className="App">
+        <div className="main-page">
+          <div className="top-section">
+            <h1>Welcome, {name}</h1>
+          </div>
+          <div className="bottom-section">
+            <button onClick={this._handleLogout}>LOGOUT</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+//In order for this component to be protected, we must wrap it with what we call a 'Higher Order Component' or HOC.
+export default app;
